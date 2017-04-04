848acbc8629db8271c5f6452cf79895fb87499d7
Chinmay Desai <chinmay@chinmay.com.au>
2017-04-04 16:33:30 +1000

    Added fetched data to tableview.

--- AlamoTable/ViewController.swift
+++ AlamoTable/ViewController.swift
@@ -13,10 +13,34 @@ import SwiftyJSON
 class AlamoTableViewController: UITableViewController {
 
     @IBOutlet var alamoView: UITableView!
+    var arrayDict = [[String: AnyObject]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Get status bar height
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        // Add insets to Tableview top
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+        
+        let endpoint = "http://api.androidhive.info/contacts/"
+        Alamofire.request(endpoint).responseJSON { (dataResponse) in
+            if dataResponse.result.value != nil {
+                let result = JSON(dataResponse.result.value!)
+                
+                if let contactData = result["contacts"].arrayObject {
+                    self.arrayDict = contactData as! [[String: AnyObject]]
+                }
+                if self.arrayDict.count > 0 {
+                    self.alamoView.reloadData()
+                }
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +48,17 @@ class AlamoTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayDict.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        var dict = arrayDict[indexPath.row]
+        cell.textLabel?.text = dict["name"] as? String
+        cell.detailTextLabel?.text = dict["phone"]?["mobile"] as? String
+        return cell
+    }
 
 }
 
