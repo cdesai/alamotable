12870e98aa218836fca7cc592925bbcdf8fb137a
Chinmay Desai <chinmay@chinmay.com.au>
2017-04-04 13:01:49 +1000

    Initial check out.

--- AlamoTable/ViewController.swift
+++ AlamoTable/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class AlamoTableViewController: UITableViewController {
 
+    @IBOutlet var alamoView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
