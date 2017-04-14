b72345cd30f29a0bd1cddf1ca353c35c58e718f8
Chinmay Desai <chinmay@chinmay.com.au>
2017-04-14 11:13:12 +1000

    Added custom cell.

--- AlamoTable/ViewController.swift
+++ AlamoTable/ViewController.swift
@@ -13,7 +13,7 @@ import SwiftyJSON
 class AlamoTableViewController: UITableViewController {
 
     @IBOutlet var alamoView: UITableView!
-    var arrayDict = [[String: AnyObject]]()
+    var arrayDict = [[String: Any]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,26 +27,21 @@ class AlamoTableViewController: UITableViewController {
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
         
-        let endpoint = "http://api.androidhive.info/contacts/"
-        Alamofire.request(endpoint).responseJSON { (dataResponse) in
-            if dataResponse.result.value != nil {
-                let result = JSON(dataResponse.result.value!)
-                
-                if let contactData = result["contacts"].arrayObject {
-                    self.arrayDict = contactData as! [[String: AnyObject]]
-                }
+        let endpoint = "https://api.spotify.com/v1/search?q=Chillstep&type=track&offset=0"
+        Alamofire.request(endpoint).validate().responseJSON { (response) in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                self.arrayDict = json["tracks"]["items"].arrayObject as! [[String : Any]]
                 if self.arrayDict.count > 0 {
                     self.alamoView.reloadData()
                 }
+            case .failure(let error):
+                print(error)
             }
-            
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return arrayDict.count
@@ -56,7 +51,13 @@ class AlamoTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
         var dict = arrayDict[indexPath.row]
         cell.textLabel?.text = dict["name"] as? String
-        cell.detailTextLabel?.text = dict["phone"]?["mobile"] as? String
+        if let artists = dict["artists"] as? [[String: Any]] {
+            if artists.count > 0 {
+                for artist in artists {
+                    cell.detailTextLabel?.text = (artist["name"] as? String)!
+                }
+            }
+        }
         return cell
     }
 
