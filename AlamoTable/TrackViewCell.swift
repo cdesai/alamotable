d4ef38fb2c9c481f9db65f7aed82bf9f82c5addc
Chinmay Desai <chinmay@chinmay.com.au>
2017-04-14 11:16:39 +1000

    added custom cell class.

---
+++ AlamoTable/TrackViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TrackViewCell.swift
+//  AlamoTable
+//
+//  Created by Chinmay Desai on 14/4/17.
+//  Copyright © 2017 Chinmay Desai. All rights reserved.
+//
+
+import UIKit
+
+class TrackViewCell: UITableViewCell {
+    
+    @IBOutlet var albumImage: UIImageView!
+    @IBOutlet var trackName: UILabel!
+    @IBOutlet var artistName: UILabel!
+}
